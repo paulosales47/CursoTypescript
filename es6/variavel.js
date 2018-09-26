b6043b0598b02119e0b2556575cdd2660e849f62
Paulo Henrique <paulo.sampaio@9425P>
2018-09-26 13:23:44 -0300

    Aula sobre hoisting em javascript

---
+++ es6/variavel.js
@@ -0,0 +1,14 @@
+var nome = "ABC"
+var nome2 = "QWE"
+const NOME_PESSOA = "Paulo"
+
+if(true)
+{
+    let nome = "DEF";
+    console.log(nome);
+
+    nome2 = "IOP"
+}
+
+console.log(nome);
+console.log(nome2);
