0df11d710ed53ef81bd31d82df117755b8f06cc4
Paulo Henrique <paulo.sampaio@9425P>
2018-09-26 13:45:39 -0300

    Utilizando arrow functions

---
+++ es6/arrow_function.js
@@ -0,0 +1,12 @@
+var dobroValor = function(numero){
+    return numero * 2;
+}
+
+console.log(dobroValor(2))
+
+
+var dobroValorArrowFunction = numero => numero*2;
+var retornaCincoArrowFunction = () => 5;
+
+console.log(dobroValorArrowFunction(2))
+console.log(retornaCincoArrowFunction())
