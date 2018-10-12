d3a4da787f2840b4e6e8fd20ccccb4006918ef96
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-10-12 17:56:15 -0300

    Testando template em unica linha

--- projetos/cursoUdemy/src/app/topo/topo.component.ts
+++ projetos/cursoUdemy/src/app/topo/topo.component.ts
@@ -2,7 +2,9 @@ import {Component } from '@angular/core'
 
 @Component({ 
     selector: 'app-topo',
-    templateUrl: './topo.component.html'
+    //templateUrl: './topo.component.html',
+    template: '<p>Componente do topo</p>'
+
 })
 
 export class TopoComponent{}
