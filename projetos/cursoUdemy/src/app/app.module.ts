802ad228fc5b7a530b7835a5090478d19918ffb9
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-10-12 18:42:19 -0300

    Criando componente progresso

--- projetos/cursoUdemy/src/app/app.module.ts
+++ projetos/cursoUdemy/src/app/app.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import { PainelComponent } from './painel/painel.component';
-import { TentativasComponent } from './tentativas/tentativas.component'
+import { TentativasComponent } from './tentativas/tentativas.component';
+import { ProgressoComponent } from './progresso/progresso.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     TopoComponent,
     PainelComponent,
-    TentativasComponent
+    TentativasComponent,
+    ProgressoComponent
   ],
   imports: [
     BrowserModule
