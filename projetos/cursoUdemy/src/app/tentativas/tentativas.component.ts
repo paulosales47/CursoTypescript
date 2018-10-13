2d25bf90f258069862b6c116c8f7dc4facfd959b
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-10-13 15:33:26 -0300

    Implementando métopdo do ciclo de vida (Onchanges) dentro do componente tentativas

--- projetos/cursoUdemy/src/app/tentativas/tentativas.component.ts
+++ projetos/cursoUdemy/src/app/tentativas/tentativas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { Coracao } from '../shared/coracao.model'
 
 @Component({
@@ -6,7 +6,11 @@ import { Coracao } from '../shared/coracao.model'
   templateUrl: './tentativas.component.html',
   styleUrls: ['./tentativas.component.css']
 })
-export class TentativasComponent implements OnInit {
+export class TentativasComponent implements OnInit, OnChanges {
+  
+  ngOnChanges(): void {
+    console.log(this.NumeroTentativasRestantes);
+  }
 
   public Coracoes: Coracao[] = [ new Coracao(true), new Coracao(true), new Coracao(true) ]
   
@@ -14,12 +18,8 @@ export class TentativasComponent implements OnInit {
   public NumeroTentativasRestantes: number;
 
   constructor() 
-  {
-    console.log(this.NumeroTentativasRestantes)
-  }
+  {}
 
-  ngOnInit() {
-    console.log(this.NumeroTentativasRestantes)
-  }
+  ngOnInit() {}
 
 }
