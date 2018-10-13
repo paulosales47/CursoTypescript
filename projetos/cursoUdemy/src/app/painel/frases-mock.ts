e5b49fb5f900dff875d1c32d8d32ac1dfe64ab0e
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-10-12 22:34:40 -0300

    Criando classe mock para as frases

---
+++ projetos/cursoUdemy/src/app/painel/frases-mock.ts
@@ -0,0 +1,7 @@
+import { Frase } from "../shared/frase.model"
+
+const FRASES: Frase[] = [
+    { FraseEng: 'I like to learn', FrasePtBr: 'Eu gosto de aprender'},
+    { FraseEng: 'How Are You', FrasePtBr: 'Como vai você'},
+    { FraseEng: 'I eat bread', FrasePtBr: 'Eu como pão'}
+]
