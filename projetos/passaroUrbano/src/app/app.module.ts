39110badc8f817757147c1e43f706a909b60c9eb
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-10-13 21:35:00 -0300

    Criando componente rodape

--- projetos/passaroUrbano/src/app/app.module.ts
+++ projetos/passaroUrbano/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import { HomeComponent } from './home/home.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopoComponent,
-    HomeComponent
+    HomeComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule
