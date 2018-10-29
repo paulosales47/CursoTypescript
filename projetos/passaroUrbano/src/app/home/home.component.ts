501345a8e2d69f12972da5c54c68328ec2c4eb9c
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-10-28 23:40:44 -0300

    Criadno um serviço e realizando injeção dele no componente home

--- projetos/passaroUrbano/src/app/home/home.component.ts
+++ projetos/passaroUrbano/src/app/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.services';
 
 @Component({
   selector: 'aws-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [OfertasService]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private oferta: OfertasService) { }
 
   ngOnInit() {
+    console.log(this.oferta.GetOfertas())
   }
 
 }
---
+++ projetos/passaroUrbano/src/app/ofertas.services.ts
@@ -0,0 +1,8 @@
+export class OfertasService{
+    public GetOfertas(): Array<string>
+    {
+        return ['Oferta 1', 'Oferta 2', 'Oferta 3']
+    }
+
+
+}
