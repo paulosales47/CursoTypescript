b28ed1123b1bbfc5d9109844cf570f9ae4935bad
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-11-03 20:52:21 -0300

    Utilizando subscribe para recuperar parametro da rota

--- projetos/passaroUrbano/src/app/oferta/oferta.component.ts
+++ projetos/passaroUrbano/src/app/oferta/oferta.component.ts
@@ -11,7 +11,8 @@ export class OfertaComponent implements OnInit {
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-    console.log(this.route.snapshot.params['id']);
+    //console.log(this.route.snapshot.params['id']);
+    this.route.params.subscribe((parametro: any) => console.log(parametro.id))
   }
 
 }
