d9f492ae800d13eea4c39f7f0c8b37d07b957809
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-11-02 21:50:50 -0300

    Criando mapeamento de rotas e utilizando a tag router-outlet

---
+++ projetos/passaroUrbano/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import {Routes, Route} from '@angular/router'
+
+import {HomeComponent} from './home/home.component'
+import {RestauranteComponent} from './restaurante/restaurante.component'
+import {DiversaoComponent} from './diversao/diversao.component'
+
+export const ROUTES: Routes = 
+[
+     {path:'', component: HomeComponent}
+    ,{path:'restaurante', component: RestauranteComponent}
+    ,{path:'diversao', component: DiversaoComponent}
+]
