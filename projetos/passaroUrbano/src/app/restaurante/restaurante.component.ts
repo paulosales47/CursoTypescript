960b3818d7840d0b468527cdb8d80f79754c99fd
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-11-02 22:09:58 -0300

    Implementando função para buscar ofertas por categoria dentro de ofertas.services e injetando o serviço de ntro de restaurante.component

--- projetos/passaroUrbano/src/app/restaurante/restaurante.component.ts
+++ projetos/passaroUrbano/src/app/restaurante/restaurante.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { OfertasService } from '../ofertas.services'
+import { Oferta } from '../shared/oferta.model';
 @Component({
   selector: 'aws-restaurante',
   templateUrl: './restaurante.component.html',
-  styleUrls: ['./restaurante.component.css']
+  styleUrls: ['./restaurante.component.css'],
+  providers: [OfertasService]
+  
 })
 export class RestauranteComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ofertaService: OfertasService) { }
 
   ngOnInit() {
+    this.ofertaService.GetOfertasCategoria('restaurante')
+    .then((ofertas: Array<Oferta>) => console.log(ofertas))
   }
 
 }
