e59eb75179bb149a98c23f7902d8f11126ff012f
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-11-04 18:14:30 -0200

    Utilizando Subject para maior controle do Observe

--- projetos/passaroUrbano/src/app/topo/topo.component.ts
+++ projetos/passaroUrbano/src/app/topo/topo.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {OfertasService} from '../ofertas.services'
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { Oferta } from '../shared/oferta.model';
+import { switchMap } from 'rxjs/operators';
 @Component({
   selector: 'aws-topo',
   templateUrl: './topo.component.html',
@@ -11,21 +12,24 @@ import { Oferta } from '../shared/oferta.model';
 export class TopoComponent implements OnInit {
 
   public ofertas: Observable<Array<Oferta>>
+  public subjectPesquisa: Subject<string> = new Subject<string>()
 
   constructor(private ofertaService: OfertasService) { }
 
   public pesquisaOferta(termoPesquisaOferta: string): void {
-    this.ofertas = this.ofertaService.PesquisaOferta(termoPesquisaOferta)
+    this.subjectPesquisa.next(termoPesquisaOferta)
 
-    this.ofertas.subscribe(
-       (ofertas: Array<Oferta>) => console.log(ofertas)
-      ,(erro: any) => console.log(erro.status)
-      ,() => console.log('Fluxo de eventos completo')
-    )
-    
   }
 
   ngOnInit() {
+    this.ofertas = this.subjectPesquisa
+    .pipe(switchMap((termoPesquisaOferta: string) => {
+      return this.ofertaService.PesquisaOferta(termoPesquisaOferta);
+    }));
+
+    this.ofertas.subscribe(
+      (ofertas: Array<Oferta>) => console.log(ofertas)
+    )
   }
 
 }
