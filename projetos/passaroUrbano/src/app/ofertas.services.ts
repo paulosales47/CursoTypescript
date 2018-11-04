581e9aad2ef529103290b58adb42b0314923059e
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-11-03 21:25:05 -0300

    Isolando url da api para um modulo a parte

--- projetos/passaroUrbano/src/app/ofertas.services.ts
+++ projetos/passaroUrbano/src/app/ofertas.services.ts
@@ -2,28 +2,31 @@ import {Oferta} from "./shared/oferta.model"
 import {Injectable} from '@angular/core'
 import {Http} from '@angular/http'
 
+import {URL_API} from './app.api'
+
 @Injectable()
 export class OfertasService{
 
-    constructor(private http: Http){}
+    constructor(private http: Http)
+    {}
 
     public GetOfertas(): Promise<Array<Oferta>>
     {
-        return this.http.get('http://localhost:3000/ofertas')
+        return this.http.get(URL_API)
         .toPromise()
         .then((resposta: any) => resposta.json())
     }
 
     public GetOfertasCategoria(categoria: string): Promise<Array<Oferta>>
     {
-        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+        return this.http.get(`${URL_API}?categoria=${categoria}`)
         .toPromise()
         .then((resposta: any) => resposta.json())
     }
 
     public GetOfertaId(id: number): Promise<Oferta>
     {
-        return this.http.get(`http://localhost:3000/ofertas?id=${id}`)
+        return this.http.get(`${URL_API}?id=${id}`)
         .toPromise()
         .then((resposta: any)=> resposta.json().shift())
     }
