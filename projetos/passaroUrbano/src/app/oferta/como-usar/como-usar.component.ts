aa9ceb47efd664dfd3fa38a409c7b2148e7c6596
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-11-03 23:17:42 -0300

    Recuperando parametros da rota pai

--- projetos/passaroUrbano/src/app/oferta/como-usar/como-usar.component.ts
+++ projetos/passaroUrbano/src/app/oferta/como-usar/como-usar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router'
 
 @Component({
   selector: 'aws-como-usar',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComoUsarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    console.log(this.route.parent.snapshot.params['id']);
+  
   }
 
 }
