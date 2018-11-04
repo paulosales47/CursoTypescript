f8b8665b429fe9dcacbb3e2093cf9e4dc851a72a
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-11-03 22:52:21 -0300

    Criando rotas filhas

--- projetos/passaroUrbano/src/app/app.routes.ts
+++ projetos/passaroUrbano/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import {HomeComponent} from './home/home.component'
 import {RestauranteComponent} from './restaurante/restaurante.component'
 import {DiversaoComponent} from './diversao/diversao.component'
 import { OfertaComponent } from './oferta/oferta.component';
+import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
+import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
 
 export const ROUTES: Routes = 
 [
@@ -11,5 +13,11 @@ export const ROUTES: Routes =
     ,{path:'restaurante', component: RestauranteComponent}
     ,{path:'diversao', component: DiversaoComponent}
     ,{path:'oferta', component: HomeComponent}
-    ,{path:'oferta/:id', component: OfertaComponent}
+    ,{path:'oferta/:id', component: OfertaComponent, 
+        children: 
+        [   
+             {path:'', component: OndeFicaComponent}
+            ,{path:'onde-fica', component: OndeFicaComponent}
+            ,{path:'como-usar', component: ComoUsarComponent}
+        ]}
 ]
