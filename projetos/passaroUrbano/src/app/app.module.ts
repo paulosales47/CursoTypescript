691c4be9d01b4cb897be5769e86e078d337e7a14
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-11-03 21:56:30 -0300

    Criando os componentes ondeFica  e comoUsar dentro do componente oferta

--- projetos/passaroUrbano/src/app/app.module.ts
+++ projetos/passaroUrbano/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { RodapeComponent } from './rodape/rodape.component';
 import { RestauranteComponent } from './restaurante/restaurante.component';
 import { DiversaoComponent } from './diversao/diversao.component';
 import { OfertaComponent } from './oferta/oferta.component';
+import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
+import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
 
 
 @NgModule({
@@ -22,7 +24,9 @@ import { OfertaComponent } from './oferta/oferta.component';
     RodapeComponent,
     RestauranteComponent,
     DiversaoComponent,
-    OfertaComponent
+    OfertaComponent,
+    ComoUsarComponent,
+    OndeFicaComponent
   ],
   imports: [
     BrowserModule,
