50014551a4593cc8a53d96b926ac9e965939b50b
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-11-15 09:10:40 -0200

    Internacionalizando componente

--- projetos/passaroUrbano/src/app/app.module.ts
+++ projetos/passaroUrbano/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule} from '@angular/http'
 import { RouterModule } from '@angular/router'
+
+import localePtBR from '@angular/common/locales/pt'
 
 import { ROUTES } from './app.routes'
 
@@ -14,6 +16,7 @@ import { DiversaoComponent } from './diversao/diversao.component';
 import { OfertaComponent } from './oferta/oferta.component';
 import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
 import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
+import { registerLocaleData } from '@angular/common';
 
 
 @NgModule({
@@ -33,7 +36,9 @@ import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+registerLocaleData(localePtBR);
