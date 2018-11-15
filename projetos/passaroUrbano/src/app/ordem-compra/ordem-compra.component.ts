436fb3a12ba5ab9c17e6059fb6e5ad6775f52488
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-11-15 21:38:31 -0200

    Adicionando validação no formulário

--- projetos/passaroUrbano/src/app/ordem-compra/ordem-compra.component.ts
+++ projetos/passaroUrbano/src/app/ordem-compra/ordem-compra.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public endereco: string = ''
+  public numero: number
+  public complemento: string = ''
+  public formaPagamento: string = '0'
+
+  public enderecoValido: boolean
+  public numeroValido: boolean
+  public complementoValido: boolean
+  public formaPagamentoValido: boolean
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public AtualizaEndereco(endereco: string): void {
+    this.endereco = endereco;
+    this.enderecoValido= this.endereco.length > 3 ? true :false
+  }
+
+  public AtualizaNumero(numero: number): void {
+    this.numero = numero
+    this.numeroValido = this.numero > -1 ? true :false
+  }
+
+  public AtualizaComplemento(complemento: string): void {}
+
+  public AtualizaFormaPagamento(formaPagamento: string): void {
+    this.formaPagamento = formaPagamento;
+    this.formaPagamentoValido = (this.formaPagamento == 'debito' || this.formaPagamento == 'dinheiro') ? true : false
   }
 
 }
