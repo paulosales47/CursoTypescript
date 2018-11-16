446c534d6fef9e9d4d5bdafafcd1c034da888f01
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-11-16 13:16:35 -0200

    Utilizando funçao decoradora ViewChild para recebimento das informações do formulário

--- projetos/passaroUrbano/src/app/ordem-compra/ordem-compra.component.ts
+++ projetos/passaroUrbano/src/app/ordem-compra/ordem-compra.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {OrdemCompraService } from './../ordem-compra.service'
 import { OrdemCompra } from '../shared/ordem-compra.model';
 import { NgForm } from '@angular/forms';
@@ -11,13 +11,15 @@ import { NgForm } from '@angular/forms';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  @ViewChild('formulario') 
+  public formulario: NgForm
 
   constructor(private ordemCompraService: OrdemCompraService) { }
 
   ngOnInit() {}
 
-  public ConfirmaCompra(formulario: NgForm): void{
-    console.log(formulario.value);
+  public ConfirmaCompra(): void{
+    console.log(this.formulario.value);
   }
 
 }
