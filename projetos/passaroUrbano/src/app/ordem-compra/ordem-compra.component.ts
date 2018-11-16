f973f8cb50886b902f2fd1e99736835e51dd2797
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-11-16 11:54:12 -0200

    Limpando componente ordem-compra para uso do template forms

--- projetos/passaroUrbano/src/app/ordem-compra/ordem-compra.component.ts
+++ projetos/passaroUrbano/src/app/ordem-compra/ordem-compra.component.ts
@@ -10,71 +10,8 @@ import { OrdemCompra } from '../shared/ordem-compra.model';
 })
 export class OrdemCompraComponent implements OnInit {
 
-  public idOrdemCompra: number
-  public endereco: string = ''
-  public numero: number
-  public complemento: string = ''
-  public formaPagamento: string = '0'
-
-  public enderecoValido: boolean
-  public numeroValido: boolean
-  public formaPagamentoValido: boolean
-
-  public enderecoEstadoPrimitivo: boolean = true
-  public numeroEstadoPrimitivo: boolean = true
-  public formaPagamentoEstadoPrimitivo: boolean = true
-  
-  public formValido: boolean = false;
-  public ordemCompra: OrdemCompra;
 
   constructor(private ordemCompraService: OrdemCompraService) { }
 
-  ngOnInit() {
-    //this.ordemCompraService.EfetivarCompra(ordemCompra);
-  }
-
-  public AtualizaEndereco(endereco: string): void {
-    this.endereco = endereco;
-    this.enderecoValido= this.endereco.length > 3 ? true :false
-    this.enderecoEstadoPrimitivo = false;
-    this.habilitaForm();
-  }
-
-  public AtualizaNumero(numero: number): void {
-    this.numero = numero;
-    this.numeroValido = this.numero > -1 ? true :false
-    this.numeroEstadoPrimitivo = false;
-    this.habilitaForm();
-  }
-
-  public AtualizaComplemento(complemento: string): void {
-    this.complemento = complemento;
-  }
-
-  public AtualizaFormaPagamento(formaPagamento: string): void {
-    this.formaPagamento = formaPagamento;
-    this.formaPagamentoValido = (this.formaPagamento == 'debito' || this.formaPagamento == 'dinheiro') ? true : false
-    this.formaPagamentoEstadoPrimitivo = false;
-    this.habilitaForm();
-  }
-
-  public habilitaForm(): void {
-
-    this.formValido =
-         this.enderecoValido 
-      && this.numeroValido 
-      && this.formaPagamentoValido;
-  }
-
-  public ConfirmaCompra(): void {
-    this.ordemCompra = new OrdemCompra(
-       this.endereco
-      ,this.numero
-      ,this.complemento
-      ,this.formaPagamento)
-
-      this.ordemCompraService.EfetivarCompra(this.ordemCompra)
-      .subscribe((idOrdemCompra: number) => this.idOrdemCompra = idOrdemCompra);
-  }
-
+  ngOnInit() {}
 }
