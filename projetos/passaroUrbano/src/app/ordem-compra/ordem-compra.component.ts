0a7dff4f0606d9a04e753bbe4c8f84c355068ccb
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-11-16 17:12:17 -0200

    Limpando componente ordem-compra para utilizaçao do ReactiveFormsModule

--- projetos/passaroUrbano/src/app/ordem-compra/ordem-compra.component.ts
+++ projetos/passaroUrbano/src/app/ordem-compra/ordem-compra.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {OrdemCompraService } from './../ordem-compra.service'
-import { OrdemCompra } from '../shared/ordem-compra.model';
-import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'aws-ordem-compra',
@@ -11,26 +9,11 @@ import { NgForm } from '@angular/forms';
 })
 export class OrdemCompraComponent implements OnInit {
 
-  @ViewChild('formulario') 
-  public formulario: NgForm
-  public idOrdemCompra: number
-
   constructor(private ordemCompraService: OrdemCompraService) { }
 
   ngOnInit() {}
 
   public ConfirmaCompra(): void{
-
-    let ordemCompra = new OrdemCompra(
-       this.formulario.value.endereco
-      ,this.formulario.value.numero
-      ,this.formulario.value.complemento
-      ,this.formulario.value.formaPagamento
-    )
-
-    this.ordemCompraService.EfetivarCompra(ordemCompra)
-    .subscribe((idOrdemCompra: number) => this.idOrdemCompra = idOrdemCompra)
-    
   }
 
 }
