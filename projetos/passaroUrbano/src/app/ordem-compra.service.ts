58bcbc77abe168ee6824e7da3ca16b1229f01d14
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-11-16 08:52:20 -0200

    Injetando serviço htpp no ordem-compra service

--- projetos/passaroUrbano/src/app/ordem-compra.service.ts
+++ projetos/passaroUrbano/src/app/ordem-compra.service.ts
@@ -1,6 +1,12 @@
+import { Injectable } from '@angular/core'
+import { Http } from '@angular/http'
 import { OrdemCompra } from './shared/ordem-compra.model'
 
+@Injectable()
 export class OrdemCompraService {
+
+    constructor(private http: Http){}
+
     public EfetivarCompra(ordemCompra: OrdemCompra){
         console.log(ordemCompra);
     }
