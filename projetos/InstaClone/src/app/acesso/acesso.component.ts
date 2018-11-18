358e374968f6ec20c11b69b16e7682fb1ae28db0
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-11-18 13:53:36 -0200

    Utilizando animação de escala

--- projetos/InstaClone/src/app/acesso/acesso.component.ts
+++ projetos/InstaClone/src/app/acesso/acesso.component.ts
@@ -14,15 +14,12 @@ import { trigger, style, state, transition, animate, keyframes } from '@angular/
     trigger('animacao-painel', 
     [
       state('criado', style({'opacity': 1})),
-      transition('void => criado', [style({'opacity': 0, transform: 'translate(100px, 0px)'}) ,animate('1000ms ease-in-out', keyframes(
+      transition('void => criado', [style({'opacity': 0, transform: 'translate(0px, 0px)'}) ,animate('1000ms ease-in-out', keyframes(
         [
-          style({offset: 0.15, opacity: 1, transform: 'translateX(0)'}),
-          style({offset: 0.25, opacity: 1, transform: 'translateY(-10px)'}),
-          style({offset: 0.40, opacity: 1, transform: 'translateY(10px)'}),
-          style({offset: 0.55, opacity: 1, transform: 'translateY(-10px)'}),
-          style({offset: 0.70, opacity: 1, transform: 'translateY(10px)'}),
-          style({offset: 0.85, opacity: 1, transform: 'translateY(-10px)'}),
-          style({offset: 1, opacity: 1, transform: 'translateY(0px)'}),
+          style({offset: 0.30, opacity: 0.3, transform: 'scale(0.3)'}),
+          style({offset: 0.45, opacity: 0.45, transform: 'scale(0.45)'}),
+          style({offset: 0.75, opacity: 0.75, transform: 'scale(0.75)'}),
+          style({offset: 0.99, opacity: 1, transform: 'scale(1)'}),
         ]
       ))])
     ])
