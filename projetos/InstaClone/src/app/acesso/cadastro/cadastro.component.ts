a58fd926260050c2b0fb3c420494e30d242d37e5
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-11-18 13:07:02 -0200

    Exibindo painel login através do clique de um link no painel cadastro

--- projetos/InstaClone/src/app/acesso/cadastro/cadastro.component.ts
+++ projetos/InstaClone/src/app/acesso/cadastro/cadastro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
+  @Output()
+  public exibirPainelLogin:EventEmitter<boolean> = new EventEmitter<boolean>()
+
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  public ExibirPainelLogin(): void{
+    this.exibirPainelLogin.emit(true);
   }
 
 }
