70f42ed53da01a7dc680d902da7c6ba465da8844
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-11-18 12:39:16 -0200

    Construindo animação para o painel de login e cadastro

--- projetos/InstaClone/src/app/acesso/acesso.component.ts
+++ projetos/InstaClone/src/app/acesso/acesso.component.ts
@@ -5,18 +5,24 @@ import { trigger, style, state, transition, animate } from '@angular/animations'
   selector: 'app-acesso',
   templateUrl: './acesso.component.html',
   styleUrls: ['./acesso.component.css'],
-  animations: [trigger('animacao-banner', [
-    state('criado', style({'opacity': 1})),
-    transition('void => criado', [
-      style({'opacity': 0, transform: 'translate(-100px, 0px)'})
-      ,animate('500ms ease-in-out')
+  animations: [
+    trigger('animacao-banner', 
+    [
+      state('criado', style({'opacity': 1})),
+      transition('void => criado', [style({'opacity': 0, transform: 'translate(-100px, 0px)'}) ,animate('500ms ease-in-out')])
+    ]),
+    trigger('animacao-painel', 
+    [
+      state('criado', style({'opacity': 1})),
+      transition('void => criado', [style({'opacity': 0, transform: 'translate(100px, 0px)'}) ,animate('500ms ease-in-out')])
     ])
-  ]
-  )]
+
+]
 })
 export class AcessoComponent implements OnInit {
 
   public estadoBanner:string = 'criado';
+  public estadoPainel:string = 'criado';
 
   constructor() { }
 
