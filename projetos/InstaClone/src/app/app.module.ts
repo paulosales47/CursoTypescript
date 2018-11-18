513d1b28e01ebb4148eab4ac0e2fc4d4acf85eab
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-11-18 01:43:55 -0200

    Criando componente cadastro

--- projetos/InstaClone/src/app/app.module.ts
+++ projetos/InstaClone/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { AcessoComponent } from './acesso/acesso.component';
 import { BannerComponent } from './acesso/banner/banner.component';
 import { LoginComponent } from './acesso/login/login.component';
+import { CadastroComponent } from './acesso/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AcessoComponent,
     BannerComponent,
-    LoginComponent
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
