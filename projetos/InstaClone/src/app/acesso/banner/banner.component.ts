f449513f14514cc5de0b81ec9ac2f9ac1636b13b
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-11-18 10:16:04 -0200

    Criando classe modelo para as imagens e importando no componente banner

--- projetos/InstaClone/src/app/acesso/banner/banner.component.ts
+++ projetos/InstaClone/src/app/acesso/banner/banner.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations';
+import { Imagem } from '../shared/imagem.model'
 
 @Component({
   selector: 'app-banner',
@@ -16,8 +17,18 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 export class BannerComponent implements OnInit {
 
   public estado: string = 'visivel'
+  public imagens: Array<Imagem>
 
-  constructor() { }
+  constructor() 
+  {
+    this.imagens = [
+      {estado: 'escondido', url: '../../../assets/banner-acesso/img_1.png'},
+      {estado: 'escondido', url: '../../../assets/banner-acesso/img_2.png'},
+      {estado: 'visivel', url: '../../../assets/banner-acesso/img_3.png'},
+      {estado: 'escondido', url: '../../../assets/banner-acesso/img_4.png'},
+      {estado: 'escondido', url: '../../../assets/banner-acesso/img_5.png'}
+    ]
+   }
 
   ngOnInit() {
   }
