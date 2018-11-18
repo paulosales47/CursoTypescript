9523ebd116cba1ba745c1cb4db5e9f4b4b45eddc
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-11-18 01:43:32 -0200

    Criando componente login

--- projetos/InstaClone/src/app/app.module.ts
+++ projetos/InstaClone/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AcessoComponent } from './acesso/acesso.component';
 import { BannerComponent } from './acesso/banner/banner.component';
+import { LoginComponent } from './acesso/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AcessoComponent,
-    BannerComponent
+    BannerComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
