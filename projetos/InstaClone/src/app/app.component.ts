b76565500958256e21ad96ee4228cdeb1e9d56ae
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-11-18 15:23:15 -0200

    Configuração inicla para o firebase no app.component

--- projetos/InstaClone/src/app/app.component.ts
+++ projetos/InstaClone/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
+  ngOnInit(): void {
+
+    let config = 
+    {
+      apiKey: "AIzaSyCgdp8zjEzeyElXWGHIocDX3xTlUP7Btc8",
+      authDomain: "app-angular-instaclone.firebaseapp.com",
+      databaseURL: "https://app-angular-instaclone.firebaseio.com",
+      projectId: "app-angular-instaclone",
+      storageBucket: "app-angular-instaclone.appspot.com",
+      messagingSenderId: "230695324120"
+    };
+
+    firebase.initializeApp(config)
+  }
+
   title = 'InstaClone';
 }
