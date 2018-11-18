6e7cfdecfc42449b587f30a0229ab751d1ed9584
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-11-18 13:45:33 -0200

    Utilizando keyframes para animações avançadas

--- projetos/InstaClone/src/app/acesso/acesso.component.ts
+++ projetos/InstaClone/src/app/acesso/acesso.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, style, state, transition, animate } from '@angular/animations';
+import { trigger, style, state, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-acesso',
@@ -14,7 +14,17 @@ import { trigger, style, state, transition, animate } from '@angular/animations'
     trigger('animacao-painel', 
     [
       state('criado', style({'opacity': 1})),
-      transition('void => criado', [style({'opacity': 0, transform: 'translate(100px, 0px)'}) ,animate('500ms ease-in-out')])
+      transition('void => criado', [style({'opacity': 0, transform: 'translate(100px, 0px)'}) ,animate('1000ms ease-in-out', keyframes(
+        [
+          style({offset: 0.15, opacity: 1, transform: 'translateX(0)'}),
+          style({offset: 0.25, opacity: 1, transform: 'translateY(-10px)'}),
+          style({offset: 0.40, opacity: 1, transform: 'translateY(10px)'}),
+          style({offset: 0.55, opacity: 1, transform: 'translateY(-10px)'}),
+          style({offset: 0.70, opacity: 1, transform: 'translateY(10px)'}),
+          style({offset: 0.85, opacity: 1, transform: 'translateY(-10px)'}),
+          style({offset: 1, opacity: 1, transform: 'translateY(0px)'}),
+        ]
+      ))])
     ])
 
 ]
