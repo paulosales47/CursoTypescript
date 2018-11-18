86ae6a7177688b65f4e1cf016703c0ab8391d3a1
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-11-18 15:30:07 -0200

    Cadastrando email e senha do usuário no firebase

--- projetos/InstaClone/src/app/auth.service.ts
+++ projetos/InstaClone/src/app/auth.service.ts
@@ -1,7 +1,11 @@
 import { Usuario } from "./acesso/shared/usuario.model";
-
+import * as firebase from 'firebase'
 export class Autenticacao{
+    
     public CadastrarUsuario(usuario: Usuario){
-        console.log(usuario);
+        firebase.auth()
+        .createUserWithEmailAndPassword(usuario.email,usuario.senha)
+        .then((resposta: any) => {console.log(resposta)})
+        .catch((erro: any) => console.log(erro))
     }
 }
