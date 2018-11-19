aa8931eaf32ed950a154ad8db633bb97b3ddde82
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-11-18 23:28:26 -0200

    Associando imagens do storage com o documento salvo

--- projetos/InstaClone/src/app/bd.service.ts
+++ projetos/InstaClone/src/app/bd.service.ts
@@ -8,18 +8,22 @@ export class BD {
     constructor(private progressoUpload: ProgressoUpload){}
 
     public SalvarNovaPublicacao(publicacao: any):void {
-
-        let nomeImagem = `${Date.now()}`
-
-        firebase.storage().ref()
-        .child(`imagens/${nomeImagem}`)
-        .put(publicacao.imagem)
-        .on(firebase.storage.TaskEvent.STATE_CHANGED,
-             (snapshot: any)=> this.progressoUpload.estado = snapshot
-            ,(erro) => this.progressoUpload.status = `Ocorreu o seguinte erro durante a publicação: ${erro}`
-            ,() => this.progressoUpload.status = 'concluido')
         
-        // firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
-        // .push({titulo: publicacao.titulo})
+
+        firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
+        .push({titulo: publicacao.titulo})
+        .then((resposta: any) => {
+            let nomeImagem = resposta.key
+
+            firebase.storage().ref()
+            .child(`imagens/${nomeImagem}`)
+            .put(publicacao.imagem)
+            .on(firebase.storage.TaskEvent.STATE_CHANGED,
+                 (snapshot: any)=> this.progressoUpload.estado = snapshot
+                 ,(erro) => this.progressoUpload.status = `Ocorreu o seguinte erro durante a publicação: ${erro}`
+                 ,() => this.progressoUpload.status = 'concluido')
+        })
+
+
     }
 }
