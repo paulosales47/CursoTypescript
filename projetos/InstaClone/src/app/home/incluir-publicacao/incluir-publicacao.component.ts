d0806d4de930c0b33bfd50de68deb7f2594a10cd
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-11-18 22:36:23 -0200

    Monitorando upload na classe incluir publicação

--- projetos/InstaClone/src/app/home/incluir-publicacao/incluir-publicacao.component.ts
+++ projetos/InstaClone/src/app/home/incluir-publicacao/incluir-publicacao.component.ts
@@ -3,6 +3,10 @@ import { FormGroup, FormControl } from '@angular/forms';
 import {BD} from '../../bd.service'
 import * as firebase from 'firebase'
 import {ProgressoUpload} from '../../progresso-upload.service'
+import { Observable, interval, observable, Subject, pipe } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+
 @Component({
   selector: 'app-incluir-publicacao',
   templateUrl: './incluir-publicacao.component.html',
@@ -35,6 +39,22 @@ export class IncluirPublicacaoComponent implements OnInit {
       ,titulo: this.formulario.value.titulo
       ,imagem: this.imagem
     });
+
+    
+    let continuaSubscribe = new Subject();
+    let acompanhaProgressoUpload = interval(1500).pipe(takeUntil(continuaSubscribe));
+    
+
+    acompanhaProgressoUpload
+    .subscribe(() => {
+      console.log(this.progressoUpload.estado);
+      console.log(this.progressoUpload.status);
+
+      if(this.progressoUpload.status === 'concluido')
+        continuaSubscribe.next(false);
+    })
+
+    
   }
 
   public PreparaIamgemUpload(event: Event): void{
